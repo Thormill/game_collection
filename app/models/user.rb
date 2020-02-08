23236a476b999cb55aedeafe5c4a43d8f1559284
Антон Шишкин <a.shishkin.b@gmail.com>
2020-02-08 15:07:01 +0300

    dynamic methods for all booleans of game - user link

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,25 @@ class User < ApplicationRecord
   has_many :user_games
   has_many :games, through: :user_games
 
-  def favor_game(game)
-    ug = user_games.find_or_initialize_by(game_id: game.id)
-    ug.update(favored: true)
+  ["own", "want", "play", "favor"].each do |method_name|
+    define_method("#{method_name}_game") do |game|
+      ug = user_games.find_or_initialize_by(game_id: game.id)
+      ug.update("#{method_name}ed".to_sym => true)
+    end
+
+    define_method("#{method_name}ed?") do |game|
+      ug = user_games.find_or_initialize_by(game_id: game.id)
+      ug.send("#{method_name}ed".to_sym)
+    end
   end
 
-  def favored?(game)
-    ug = user_games.find_or_initialize_by(game_id: game.id)
-    ug.favored
-  end
+  # def favor_game(game)
+  #   ug = user_games.find_or_initialize_by(game_id: game.id)
+  #   ug.update(favored: true)
+  # end
+
+  # def favored?(game)
+  #   ug = user_games.find_or_initialize_by(game_id: game.id)
+  #   ug.favored
+  # end
 end
